5434d4b99675be5887591be984afa15c39d14976
Arad <75213466+aradjpy@users.noreply.github.com>
2025-10-06 02:36:01 -0400

    Update PunchInOut.py
    This version of the responsibility tracker is an updated version where now you can actually write an explanation to your work and whenever needed see the top hours you've worked, and when you choose a certain hour and click on the designated button it shows you what you've been working on during that time :)

--- PunchInOut.py
+++ PunchInOut.py
@@ -1,9 +1,11 @@
 from tkinter import *
 import time as t
 from datetime import datetime as dt
-# import pandas as pd
 import numpy as np
 import os
+from tkinter import messagebox
+import math
+import re
 
 root=Tk()
 
@@ -31,6 +33,59 @@ timeData = None
 secondsPassed = 0
 minutesPassed = 0
 hoursPassed = 0
+leadList = []
+lList = None
+dataL = {}
+lastindex = 0
+alreadyStarted = False
+
+def get_exp():
+    global dataL
+    print(lList.curselection())
+    listedDataL = list(dataL.items())
+
+    said=listedDataL[lList.curselection()[0]][1]
+    messagebox.showinfo(title='Information', message=said)
+    # OK, this is the game plan, you change the structure, so start introducing the explaination with the timers in the same text file, seprated by ~, now whenever the user inputs an explaination it also adds ~THE EXPLAINATION
+    # Now we make that a dictionary, and then apply dict(sorted(dictionary.items())) then we make a list of all the dictionaries
+    # then everytime the user clicks on either one, it takes the index of the list that's clicked and then chooses that one but this time with the explaination, and it also makes sure the ones with no explaination are just empty or with space that get's returned :)
+    
+    
+
+def leaderBoard():
+    global lList
+    global lastindex
+    global dataL
+
+    lb = Tk()
+    lb.title('Leader Board')
+    lb.geometry('500x500')
+    lb.resizable(0,0)
+    fileInfo = ''
+    lList=Listbox(lb,fg="black",font=("Aerial", 14, "bold italic"))
+    show_explain = Button(lb, text='Show explaination', command=get_exp)
+    try:
+        with open(os.path.dirname(__file__) + '/times.txt', 'r') as f:
+            fileInfo = f.read()
+            fileInfo = re.split(r'[~\n]',fileInfo)
+        fTime = [fileInfo[i] for i in range(len(fileInfo)) if fileInfo[i] != '']
+        lstT = []
+        for i in range(0,len(fTime) - 1,2):
+            lstT.append([fTime[i], fTime[i+1]])
+        dataL.update(lstT)
+        fTime = list(dataL.keys())
+        fTime = [int(math.ceil(float(fTime[i]))) for i in range(len(fTime)) if fTime[i] != '']
+        fTime.sort(reverse=True)
+        dataL = dict(sorted(dataL.items(), key=lambda x: math.ceil(float(x[0])), reverse=True))
+        lastindex = len(fTime) - 1
+        [lList.insert(END,str(i) + ' Seconds') for i in fTime]
+        lList.pack()
+    except:
+        messagebox.showerror(title='Not Found', message='Please make sure you have used the timer before :)')
+    show_explain.pack()
+    return fileInfo
+
+    lb.mainloop()
 
 def timer():
     global isPunchedIn
@@ -59,32 +114,58 @@ def timer():
         arr[0:2] = list('0' + str(hoursPassed)) if len(str(hoursPassed)) < 2 else list(str(hoursPassed))
         TimerLable.config(text=''.join(arr))
     timeData = root.after(1000,timer)
-    # print(timeData)
 
 
 def punchIn():
     global isPunchedIn
     global thisTime
     global secondsPassed
+    global timeData
 
     if isPunchedIn == False:
         isPunchedIn=True
         try:
-            with open(os.path.dirname(__file__) + '/punchIndate.txt', 'r+') as f:
-                f.truncate(0)
+            with open(os.path.dirname(__file__) + '/punchIndate.txt', 'w') as f:
                 f.write(dt.now())
         except:
             with open(os.path.dirname(__file__)  + '/punchIndate.txt', 'w') as f:
                 f.write(str(dt.now()))
     
-    thisTime = t.time()
-    secondsPassed = 0
-    timer()
-    # remember to start threading in order to do your timer :-)
+        thisTime = t.time()
+        secondsPassed = 0
+        timer()
+    else:
+        m = messagebox.askokcancel(title='timer already started!', message='You already have a timer, you sure you want to restart?')
+        if m:
+            thisTime = t.time()
+            secondsPassed = 0
+            root.after_cancel(timeData)
+            timer()
+
+# def secPunchOut():
+
 
 def punchOut():
     global timeData
     isPunchedIn = False
+    global explaination
+    preTime = t.time()
+    timeSpent = preTime - thisTime
+
+    try:
+        eg = str(explaination.get())
+        if eg == '':
+            eg = 'None :('
+    except:
+        eg = 'None :('
+    
+
+    with open(os.path.dirname(__file__) + '/times.txt', 'a+') as f:
+        f.write(str(timeSpent) + '~' + eg + '\n')
+
+    # if len(explaination.get()) > 1:
+    #     with open(os.path.dirname(__file__) + '/explain.txt', 'a') as f:
+    #         f.write(explaination.get())
     root.after_cancel(timeData)
 
 
@@ -105,9 +186,16 @@ TimerLable.pack(expand=True)
 bottmSec = Frame(root)
 bottmSec.grid(row=2,column=0,sticky='nsew')
 
+bottmSec.rowconfigure(0,weight=100)
+bottmSec.columnconfigure(0,weight=50)
+bottmSec.columnconfigure(1,weight=50)
 
-leadWorkingTimes = Button(bottmSec, text='leaderboard')
-leadWorkingTimes.pack(anchor='center',fill='both',expand=True)
+
+leadWorkingTimes = Button(bottmSec, text='leaderboard',command=leaderBoard)
+leadWorkingTimes.grid(row=0,column=0,sticky='nsew' )
 print(os.getcwd() + '/punchIndate.txt')
+
+explaination = Entry(bottmSec)
+explaination.grid(row=0,column=1,sticky='nsew')
 
 root.mainloop()
